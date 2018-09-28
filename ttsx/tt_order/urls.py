8effa86477b7fcea151ac8e527be9da22c8ef353
yunlong <909576924@qq.com>
2018-09-28 23:48:13 +0800

    订单模块优化完成

--- ttsx/tt_order/urls.py
+++ ttsx/tt_order/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+# coding=utf-8
 import views
+from django.conf.urls import url
 
 urlpatterns = [
-    url(r"^orderinfo/?(\d*)/?(\d*)$",views.orderInfo),
-    url(r"^ordercl/?(\d*)$",views.ordercl),
+    url(r"^orderinfo/?(\d*)/?(\d*)$",views.orderInfo),              # 提交订单页面
+    url(r"^ordercl/?(\d*)$",views.ordercl),                         # 订单处理
 ]
